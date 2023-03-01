4d798a1ca15f1560ecb04fb28cd385147ce0fb30
kawakionrails <erick_nara@icloud.com>
2023-03-01 11:02:37 -0300

    stop: app was not running - fixed Hilt compiler dependency

--- app/src/main/java/io/github/kawaki/naha/presenter/fragments/home/HomeViewModel.kt
+++ app/src/main/java/io/github/kawaki/naha/presenter/fragments/home/HomeViewModel.kt
@@ -2,9 +2,10 @@ package io.github.kawaki.naha.presenter.fragments.home
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.kawaki.naha.presenter.base.BaseViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : BaseViewModel() {
+class HomeViewModel @Inject constructor() : BaseViewModel() {
 
     // TODO: Not yet implemented
 
